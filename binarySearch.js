4d4dc5c81493f65c9c6106132bd883fbc74cc2b4
NihalDixit <nhldxt026@gmail.com>
2025-01-28 15:45:44 +0530

    implemented binary search using recursion

--- binarySearch.js
+++ binarySearch.js
@@ -18,6 +18,23 @@ function binarySearch(target, array) {
   }
   return -1;
 }
+
+// binary search using recursive approach.
+function binarySearchRecursive(array, target, low, high) {
+  if (low > high) {
+    return -1;
+  }
+  const mid = Math.floor((low + high) / 2);
+  if (array[mid] === target) {
+    return mid;
+  } else if (array[mid] < target) {
+    return binarySearchRecursive(array, target, mid + 1, high);
+  } else {
+    return binarySearchRecursive(array, target, low, mid - 1);
+  }
+}
+
 const sortedArray = [2, 4, 6, 8, 10, 12, 14];
 const target = 10;
-console.log(binarySearch(target, sortedArray));
+//console.log(binarySearch(target, sortedArray));
+console.log(binarySearchRecursive(sortedArray, target, 0, sortedArray.length));
